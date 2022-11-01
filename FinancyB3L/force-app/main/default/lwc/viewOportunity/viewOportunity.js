d6f3fa13a93c8ae491a50b45adc2b308b70705cc
B3rnal <angelogomesfranca@gmail.com>
2022-11-01 14:27:40 -0300

    getOpportunitys
    Dev LWC getOpportunity relacionado a account

--- FinancyB3L/force-app/main/default/lwc/viewOportunity/viewOportunity.js
+++ FinancyB3L/force-app/main/default/lwc/viewOportunity/viewOportunity.js
@@ -1,9 +1,11 @@
 import { LightningElement, wire, api } from 'lwc';
 import viewOportunities from '@salesforce/apex/OpportunityController.getOportunity';
 import { getRecord } from 'lightning/uiRecordApi';
+import StageName from '@salesforce/schema/Opportunity.StageName';
 
 const columns = [
-    { label: 'Label', fieldName: 'name' },
+    { label: 'Label', fieldName: 'oppUrl', type: 'url', typeAttributes: {label: {fieldName: 'Name' }, target: '_self'} },
+    
     { label: 'Stage', fieldName: 'StageName'}
 ];
     
@@ -11,18 +13,27 @@ const columns = [
 export default class ViewOportunity extends LightningElement {
     
     @api recordId;
+    oportunityData;
 
+    get oppData(){
+        return this.oportunityData && this.oportunityData.length > 0 ;
+    }
+
+    columns = columns;
+    
     @wire(viewOportunities,{accountId: '$recordId'})
     wiredOportunities({error,data}){
         if(error){
             console.log(JSON.stringify(error));
             
         }else if(data){
-            
-            columns = columns;
-            console.log(JSON.stringify(data));
-
-            
+            let dataList = JSON.parse(JSON.stringify(data));
+            dataList.forEach(element => {
+                element.oppUrl = '/' + element.Id;
+            });
+            this.oportunityData = dataList;
+            console.log(JSON.stringify(this.oportunityData));
+            console.log(JSON.stringify(columns));
         };
         
     };
